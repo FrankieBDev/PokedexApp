9d7e17840d3c77aae2797f9994faeac22fdefde3
Frankie <francesca.beckingham@booking.com>
2025-04-07 09:29:01 +0100

    Api Connected

---
+++ app/src/main/java/com/example/pokedexapp/data/model/PokemonResponse.kt
@@ -0,0 +1,12 @@
+package com.example.pokedexapp.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PokemonResponse(
+    @SerializedName("results") val results: List<PokemonResult>
+)
+
+data class PokemonResult(
+    @SerializedName("name") val name: String,
+    @SerializedName("url") val url: String
+)
--- app/src/main/java/com/example/pokedexapp/ui/screen/PokedexScreen.kt
+++ app/src/main/java/com/example/pokedexapp/ui/screen/PokedexScreen.kt
@@ -1,48 +1,56 @@
 package com.example.pokedexapp.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pokedexapp.ui.viewModel.PokedexUiState
 import com.example.pokedexapp.ui.viewModel.PokedexViewModel
 
 @Preview
 @Composable
 fun PokedexScreen(viewModel: PokedexViewModel = viewModel()) {
-    val pokemonList by viewModel.pokemonList.collectAsState()
+    val uiState by viewModel.uiState.collectAsState()
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(100.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(onClick = { viewModel.fetchPokemonList() }) {
-                Text("Load Pokemon")
-            }
-
-            if (pokemonList.isEmpty()) {
-                Text("No pokemon found")
-            } else {
-                LazyColumn {
-                    items(pokemonList) { pokemonList ->
-                        Text(text = pokemonList, modifier = Modifier.padding(8.dp))
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        when (uiState) {
+            is PokedexUiState.Success -> {
+                LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                    items((uiState as PokedexUiState.Success).pokemonList) { pokemon ->
+                        Text(text = pokemon, modifier = Modifier.padding(8.dp))
                     }
                 }
             }
+            is PokedexUiState.Loading -> {
+                CircularProgressIndicator()
+            }
+            is PokedexUiState.Error -> {
+                val errorMessage = (uiState as PokedexUiState.Error).message
+                Text(text = errorMessage, color = Color.Red)
+            }
         }
+
+        Button(onClick = { viewModel.fetchPokemonList() }) {
+            Text("Load Pokemon")
+        }
+    }
 }
